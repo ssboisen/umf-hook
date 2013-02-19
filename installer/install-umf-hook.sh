6c2547cd3f701caa354d1451f15b147e1580df17
Simon Skov Boisen <simon@skov-boisen.dk>
2013-02-19 10:04:55 +0100

    Update installer/install-umf-hook.sh

--- installer/install-umf-hook.sh
+++ installer/install-umf-hook.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-curl -L -o .git/hooks/umf-hook.exe https://github.com/downloads/ssboisen/umf-hook/umf-hook.exe
-curl -L -o .git/hooks/pre-commit https://raw.github.com/ssboisen/umf-hook/master/githook/pre-commit
+curl -s -L -o .git/hooks/umf-hook.exe https://github.com/downloads/ssboisen/umf-hook/umf-hook.exe
+curl -s -L -o .git/hooks/pre-commit https://raw.github.com/ssboisen/umf-hook/master/githook/pre-commit
 echo "umf-hook has been installed"
